332e371e6ec55c2075c92d681d5dbcea942a1ea4
John Agyemang <oseiagyemanjohn@gmail.com>
2025-08-29 17:44:15 +0000

    domain

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -172,8 +172,7 @@ AWS_S3_ENDPOINT_URL = f"https://{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
 
 # # CDN domain for serving public files
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_REGION_NAME}.cdn.digitaloceanspaces.com"
-AWS_MEDIA_LOCATION = 'media'
-AWS_LOCATION = 'static'
+AWS_LOCATION = 'media'
 
 AWS_DEFAULT_ACL = "public-read"
 AWS_QUERYSTRING_AUTH = False
